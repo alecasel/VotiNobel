581626f0e9a42e0525ffb9007cf02b5e0000e480
Alessandro <Alessandro@ALE-ACERSPIN>
2021-04-16 17:15:40 +0200

    Non funziona con 50 crediti

--- src/main/java/it/polito/tdp/nobel/model/Model.java
+++ src/main/java/it/polito/tdp/nobel/model/Model.java
@@ -1,16 +1,73 @@
 package it.polito.tdp.nobel.model;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-public class Model {
+import it.polito.tdp.nobel.db.EsameDAO;
 
+public class Model {
 	
-	public Set<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
-		System.out.println("TODO!");
-		return null;	
+	private List<Esame> partenza;
+	private Set<Esame> soluzioneMigliore;
+	private double mediaSoluzioneMigliore;
+	int L = 0;
+	
+	public Model() {
+		EsameDAO dao = new EsameDAO();
+		this.partenza = dao.getTuttiEsami();
+		//	Soluzione migliore non qui perché altrimenti dovrei pulirla ogni volta
 	}
 
 	
+	public Set<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
+		
+		Set<Esame> parziale = new HashSet<Esame>();
+		soluzioneMigliore = new HashSet<Esame>();
+		mediaSoluzioneMigliore = 0;
+		
+		cerca(parziale, 0, numeroCrediti);
+		
+		return soluzioneMigliore;	
+	}
+
+	
+	private void cerca(Set<Esame> parziale, int i, int m) {
+		//	casi terminali
+		
+		int crediti = sommaCrediti(parziale);
+		
+		if (crediti > m) {
+			return ;
+		}
+		
+		if (crediti == m) {
+			double media = calcolaMedia(parziale);
+			if (media > mediaSoluzioneMigliore) {
+				soluzioneMigliore = new HashSet<>(parziale);
+				mediaSoluzioneMigliore = media;
+			}
+			
+			return ;	//	Ho trovato la soluzione migliore, non vado oltre
+		}
+		
+		//	sicuramente crediti < m
+		//	L = N => non ci sono più esami da aggiungere
+		if (L == partenza.size()) {
+			return ;	//	non posso aggiungere esami perché non ne ho più
+		}
+		
+		//	Genero i sottoproblemi
+		for (Esame esame : partenza) {
+			if (!parziale.contains(esame)) {
+				parziale.add(esame);
+				cerca(parziale, L+1, m);
+				parziale.remove(esame);
+			}
+		}
+	}
+
+
 	public double calcolaMedia(Set<Esame> esami) {
 		
 		int crediti = 0;
